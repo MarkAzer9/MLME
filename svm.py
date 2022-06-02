61ed8d990d0926a43cf00a071abf280dae250a35
MarkAzer9 <106721744+MarkAzer9@users.noreply.github.com>
2022-06-02 08:11:09 +0200

    Add files via upload

---
+++ svm.py
@@ -0,0 +1,19 @@
+
+from sklearn.metrics import accuracy_score,confusion_matrix
+from sklearn.svm import SVC
+from utils.dataset import read_dataset,flatten
+
+svm = SVC(kernel='rbf',C=100,gamma=0.01,degree=9,verbose=True)
+
+train_data,train_labels=read_dataset('train',100)
+train_data=flatten(train_data)
+
+svm.fit(train_data, train_labels)
+
+test_data,test_labels=read_dataset('test',100)
+test_data=flatten(test_data)
+
+pred = svm.predict(test_data)
+print(accuracy_score(test_labels, pred)) # Accuracy
+confusion=confusion_matrix(test_labels,pred)
+
