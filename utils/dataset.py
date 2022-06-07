7cbb66d678748d741fa9fdc9657c0a06cb3cef8a
AyaBassel3 <87432856+AyaBassel3@users.noreply.github.com>
2022-06-07 14:58:25 +0200

    Add files via upload

--- utils/dataset.py
+++ utils/dataset.py
@@ -21,15 +21,21 @@ def read_dataset(data_type,dim):
                 img_path=os.path.join(symbol_dir, img)
                 img=cv2.imread(img_path)
                 gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                res=cv2.resize(gray, dsize=(dim, dim), interpolation=cv2.INTER_CUBIC)
+                kernel_aya = np.ones((5, 5), np.float32) / 25
+                dst = (255-(cv2.filter2D(gray, -1, kernel_aya)))
+                res=cv2.resize(dst, dsize=(dim, dim), interpolation=cv2.INTER_CUBIC)
                 img_count=img_count+1
+                lbl_out = np.zeros(10)
+                lbl_out[int(symbol)] = 1
                 print(img_count)
                 data_set.append(res)
-                labels.append(int(symbol))
+                labels.append(lbl_out)
 
-    data=np.array(data_set)/255
+    data=np.array(data_set)/255 #np array ashan feeh features ktiir
     labels=np.array(labels)
+    np.save('data_set',data)
+    np.save('labels',labels)
     return data,labels
 
 def flatten(data):
-    return np.reshape(data,[-1,data.shape[1]*data.shape[2]])
+    return np.reshape(data,[-1,data.shape[1]*data.shape[2]]) #1d array
