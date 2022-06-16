ed32762b50ff982e8cc5a1463d227f66bc2c2028
MarkAzer9 <106721744+MarkAzer9@users.noreply.github.com>
2022-06-16 21:44:15 +0200

    Update svm.py

--- svm.py
+++ svm.py
@@ -7,11 +7,13 @@ svm = SVC(kernel='rbf',C=100,gamma=0.01,degree=9,verbose=True)
 
 train_data,train_labels=read_dataset('train',100)
 train_data=flatten(train_data)
+train_labels=np.argmax(train_labels,1)
 
 svm.fit(train_data, train_labels)
 
 test_data,test_labels=read_dataset('test',100)
 test_data=flatten(test_data)
+test_labels=np.argmax(test_labels,1)
 
 pred = svm.predict(test_data)
 print(accuracy_score(test_labels, pred)) # Accuracy
